e1c81c8043896422aab1f0de106ffe05174ad9f5
Juan Londoño <jelondono@sura.com.co>
2020-05-09 10:58:56 -0500

    Implementando diseño de Login

---
+++ CedesistemasApp/Views/LoginPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CedesistemasApp.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+#if DEBUG
+            txt_email.Text = "admin@admin.com";
+            txt_password.Text = "admin";
+#endif
+        }
+
+        void btn_login_Clicked(System.Object sender, System.EventArgs e)
+        {
+            string email = txt_email.Text;
+            string password = txt_password.Text;
+
+            if(email == "admin@admin.com" && password == "admin")
+            {
+
+            }
+            else
+            {
+                DisplayAlert("Error al ingresar", "El usuario y/o contraseña no son correctos", "Aceptar");
+            }
+        }
+    }
+}
